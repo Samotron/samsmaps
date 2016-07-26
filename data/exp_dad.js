4373574865a70bd2e49278376f3dda6622870ec4
Sam Rogers <james.s.rogers23@gmail.com>
2016-07-26 10:23:49 +0100

    Initial commit;

---
+++ data/exp_dad.js
@@ -0,0 +1,11 @@
+var exp_dad = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                                                                  
+"features": [
+{ "type": "Feature", "properties": { "ddd": "We are here!" }, "geometry": { "type": "Point", "coordinates": [ -2.469195001823979, 51.499217116892154 ] } },
+{ "type": "Feature", "properties": { "ddd": "Temporary traffic lights" }, "geometry": { "type": "Point", "coordinates": [ -2.469800370924661, 51.498934472816337 ] } },
+{ "type": "Feature", "properties": { "ddd": "Park at side of road here" }, "geometry": { "type": "Point", "coordinates": [ -2.469262265057388, 51.499709122768763 ] } },
+{ "type": "Feature", "properties": { "ddd": "Postcode here: BS16 7GG" }, "geometry": { "type": "Point", "coordinates": [ -2.470069423858297, 51.49843199013138 ] } }
+]
+}
